f287ed689d38ede24043b281a19500634d2050d8
Pepe Valdivia <jvaldivia@softweb.pe>
2017-09-13 10:52:57 -0500

    Todas las respuestas por default text/html; charset=utf-8

--- handlers/usuario_handler.go
+++ handlers/usuario_handler.go
@@ -4,7 +4,6 @@ import "net/http"
 import "github.com/martini-contrib/sessions"
 
 func Index(res http.ResponseWriter, req *http.Request, session sessions.Session) (int, string) {
-	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-    var rpta string = "usuario/index XD"
+    var rpta string = "hola mundo"
 	return 200, rpta
 }
--- middlewares/app_headers.go
+++ middlewares/app_headers.go
@@ -5,4 +5,5 @@ import "net/http"
 func Headers(res http.ResponseWriter, req *http.Request) () {
 	res.Header().Set("Server", "Ubuntu")
 	res.Header().Set("X-Powered-By", "Go")
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
 }
